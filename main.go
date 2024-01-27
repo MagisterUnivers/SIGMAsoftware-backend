3a58a84d629b4b07f1fbf92de7428be5c9b32597
Andrey Shaposhnikov <droniiiandruxa@gmail.com>
2024-01-27 11:42:16 +0200

    ADD: Some users to "database"

--- main.go
+++ main.go
@@ -18,6 +18,10 @@ var users = make(map[uuid.UUID]User)
 func main() {
 	router := gin.Default()
 
+	users["1"] = User{ID: "1", Name: "John Wood", Email: "john@example.com", Notes: "Eg: Famous example guy"}
+  users["2"] = User{ID: "2", Name: "Jane Foster", Email: "jane@example.com", Notes: "The one with the hammer"}
+  users["3"] = User{ID: "3", Name: "Jackson Storm", Email: "jackson@example.com", Notes: "Not McQueen"}
+
 	router.GET("/users", getUsers)
 	router.POST("/users", createUser)
 	router.GET("/users/:id", getUser)
