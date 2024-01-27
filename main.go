6c97061b4190752b68a65f06dd44f720dea19700
Andrey Shaposhnikov <droniiiandruxa@gmail.com>
2024-01-27 12:01:47 +0200

    ADD: Create updateUser func

--- main.go
+++ main.go
@@ -66,3 +66,25 @@ func getUser(c *gin.Context) {
 
 	c.JSON(http.StatusOK, user)
 }
+
+func updateUser(c *gin.Context) {
+	userID, err := uuid.Parse(c.Param("id"))
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid user ID format"})
+		return
+	}
+
+	user, exists := users[userID]
+	if !exists {
+		c.JSON(http.StatusNotFound, gin.H{"error": "User not found"})
+		return
+	}
+
+	if err := c.BindJSON(&user); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	users[userID] = user
+	c.JSON(http.StatusOK, user)
+}
